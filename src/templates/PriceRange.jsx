c932472e43be9e199bcc03522e7a6b406ea39434
sofiehilge <sofie.hilge@gmail.com>
2023-10-26 11:39:44 +0200

    add range slider

--- src/templates/PriceRange.jsx
+++ src/templates/PriceRange.jsx
@@ -1,14 +1,33 @@
 import tw from "tailwind-styled-components";
+import {useState, useEffect} from "react"
 
 const StyledColumn = tw.div`
 flex
 flex-column`;
 
-const PriceRange = () => {
+const PriceRange = (min, max) => {
+
+    const [priceRange, setPriceRange] = useState();
+    console.log(priceRange);
+    /* ------------------get all homes--------------- */
+    useEffect(() => {
+      const getPriceRange = {
+        method: "GET",
+        headers: {
+          accept: "application/json",
+        },
+      };
+  
+      fetch(`https://dinmaegler.onrender.com/homes?price_gte=${min}&price_lt=${max}`, getPriceRange)
+        .then((response) => response.json())
+        .then((response) => setPriceRange(response))
+        .catch((err) => console.error(err));
+    }, []);
+
   return (
     <StyledColumn>
-        <p></p>
-      <input type="range" min="1" max="100" />
+        <p>Pris-interval</p>
+      <input type="range" min="0 kr." max="12.000.000 kr." value="100.000"/>
     </StyledColumn>
   );
 };

8d7e89ef7bf320c524d7cc0a09aae8b804721b51
sofiehilge <sofie.hilge@gmail.com>
2023-10-27 10:08:16 +0200

    add styling to slectedpropertylist in home

--- src/templates/SelectedPropertyList.jsx
+++ src/templates/SelectedPropertyList.jsx
@@ -1,14 +1,17 @@
 import tw from "tailwind-styled-components";
+import PropertyListCard from "../templates/PropertyListCard"
 
 const StyledArticle = tw.article`
 flex
 justify-center
+flex-col
 `;
 const SelectedPropertyList = () => {
   return (
     <StyledArticle>
       <h2>Udvalgte Boliger</h2>
       <p>There are many variations of passages of Lorem Ipsum available but the this in majority have suffered alteration in some</p>
+      <PropertyListCard/>
     </StyledArticle>
   );
 };
